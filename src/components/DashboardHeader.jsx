06006896ec017f371426bcbed9a18481401611c1
Pratik K Tiwari <ipratiktiwari@gmail.com>
2021-09-13 18:57:54 +0530

    added location change on logout

--- src/components/DashboardHeader.jsx
+++ src/components/DashboardHeader.jsx
@@ -17,6 +17,7 @@ export default class DashboardHeader extends React.Component {
   }
   logOutUser = () => {
     document.cookie = "userEmail=;expires=Thu, 01 Jan 1970 00:00:01 GMT;";
+    window.location.href = "/login";
   };
   render() {
     const { userEmailAlias } = this.state;
